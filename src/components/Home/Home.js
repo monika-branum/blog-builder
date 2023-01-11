792e970fae018718b1ab9829e650b533c6431771
Monika <moncobra@comcast.net>
2023-01-11 14:56:40 -0800

    most of features finished, but display is not working yet

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Preview from '../Preview/Preview';
 import Editor from '../Editor/Editor';
@@ -6,13 +6,31 @@ import Editor from '../Editor/Editor';
 import './Home.css';
 
 export default function Home() {
+  const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
+  const [titleFont, setTitleFont] = useState('titillium');
+  const [align, setAlign] = useState('center');
+  const [text, setText] = useState('');
   // add useState calls here for title, subtitle, font, align, and text
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview {...{ title, subtitle, titleFont, align, text }} />
+      <Editor
+        {...{
+          title,
+          setTitle,
+          titleFont,
+          setTitleFont,
+          subtitle,
+          setSubtitle,
+          align,
+          setAlign,
+          text,
+          setText,
+        }}
+      />
     </main>
   );
 }
